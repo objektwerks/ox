d6790c7eed6d4a531511ab72fbbc5e793c29a3f7
objektwerks <objektwerks@runbox.com>
2023-09-21 08:59:27 -0400

    added supervised test

---
+++ src/test/scala/objektwerks/SupervisedTest.scala
@@ -0,0 +1,18 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import ox.*
+
+import FileLineCountTask.*
+
+class SupervisedTest extends AnyFunSuite with Matchers:
+  test("supervised > fork") {
+    val lineCount = supervised {
+      val alines: Fork[Int] = fork( countLines("./data/data.a.csv") )
+      val blines: Fork[Int] = fork( countLines("./data/data.b.csv") )
+      alines.join() + blines.join()
+    }
+    lineCount shouldBe expectedLineCount
+  }
