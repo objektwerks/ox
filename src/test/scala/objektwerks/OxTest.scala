657755668194cc53ef4ee2245a42d34b92429702
objektwerks <objektwerks@runbox.com>
2023-09-21 08:57:48 -0400

    refactored ox test

--- src/test/scala/objektwerks/OxTest.scala
+++ src/test/scala/objektwerks/OxTest.scala
@@ -41,31 +41,3 @@ class OxTest extends AnyFunSuite with Matchers:
     }
     lineCount shouldBe expectedLineCount
   }
-
-  test("scoped > fork > channel > map") {
-    scoped {
-      val channel = Channel[Int]()
-      fork {
-        channel.send(2)
-        channel.send(4)
-        channel.done()
-      }
-      channel
-        .map(i => i * i)
-        .foreach(i => println(s"*** channel map squared $i by ${Math.sqrt(i).toInt}"))
-    }
-  }
-
-  test("supervised > fork > channel > transform") {
-    supervised {
-      val channel = Channel[Int]()
-      fork {
-        channel.send(6)
-        channel.send(8)
-        channel.done()
-      }
-      channel
-        .transform(_.filter(i => i % 2 == 0).map(i => i * i))
-        .foreach(i => println(s"*** channel transform squared $i by ${Math.sqrt(i).toInt}"))
-    }
-  }
