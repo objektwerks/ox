6f08b3cbfccc519fc0dbc97d68fcfd8c24ab253c
objektwerks <objektwerks@runbox.com>
2024-03-29 15:03:20 -0400

    added fibonacci

---
+++ src/test/scala/objektwerks/Fibonacci.scala
@@ -0,0 +1,12 @@
+package objektwerks
+
+object Fibonacci {
+    def fibonacci(n: Long): BigInt =
+    @tailrec
+    def loop(n: Long, a: Long, b: Long): BigInt =
+      n match
+        case 0 => a
+        case _ => loop(n - 1, b, a + b)
+
+    loop(n, 0, 1)
+}
