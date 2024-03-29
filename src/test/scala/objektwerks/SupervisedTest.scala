1691b09ab0e3be46364a0827538e64e9a45322e4
objektwerks <objektwerks@runbox.com>
2024-03-29 14:52:16 -0400

    edited supervised test

--- src/test/scala/objektwerks/SupervisedTest.scala
+++ src/test/scala/objektwerks/SupervisedTest.scala
@@ -10,8 +10,8 @@ import FileLineCount.*
 final class SupervisedTest extends AnyFunSuite with Matchers:
   test("supervised > fork"):
     val lineCount = supervised {
-      val alines: Fork[Int] = fork( countLines("./data/data.a.csv") )
-      val blines: Fork[Int] = fork( countLines("./data/data.b.csv") )
+      val alines: Fork[Int] = fork( countLines(aFile) )
+      val blines: Fork[Int] = fork( countLines(bFile) )
       alines.join() + blines.join()
     }
     lineCount shouldBe expectedLineCount
