daf44d1a2c452f510065e847044323e06ebfcee1
objektwerks <objektwerks@runbox.com>
2024-03-29 14:53:24 -0400

    edited scoped test

--- src/test/scala/objektwerks/ScopedTest.scala
+++ src/test/scala/objektwerks/ScopedTest.scala
@@ -12,8 +12,8 @@ import FileLineCount.*
 final class ScopedTest extends AnyFunSuite with Matchers:
   test("scoped > fork"):
     val lineCount = scoped {
-      val alines: Fork[Int] = fork( countLines(aFile) )
-      val blines: Fork[Int] = fork( countLines(bFile) )
+      val alines = fork( countLines(aFile) )
+      val blines = fork( countLines(bFile) )
       alines.join() + blines.join()
     }
     lineCount shouldBe expectedLineCount
