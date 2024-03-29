05f648e50beca06d71dae5e31c834d4093c52d33
objektwerks <objektwerks@runbox.com>
2024-03-29 18:09:49 -0400

    added race either test

--- src/test/scala/objektwerks/RaceTest.scala
+++ src/test/scala/objektwerks/RaceTest.scala
@@ -11,3 +11,10 @@ final class RaceTest extends AnyFunSuite with Matchers:
   test("race"):
     val aOrb = race( countFileLines(aFile), countFileLines(bFile) )
     assert( aOrb == aFileLineCount || aOrb == bFileLineCount )
+
+  test("race either"):
+    val result = raceEither(
+      Right( countFileLines(aFile) ),
+      Left(-1)    
+    )
+    result.map { result => result shouldBe aFileLineCount }
