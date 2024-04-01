4cbc0180ebd87fd964b62501f57b01556d3219de
objektwerks <objektwerks@runbox.com>
2024-04-01 13:55:01 -0400

    edited retry test

--- src/test/scala/objektwerks/RetryTest.scala
+++ src/test/scala/objektwerks/RetryTest.scala
@@ -3,6 +3,6 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class RetryTest extends AnyFunSuite with Matchers:
+final class RetryTest extends AnyFunSuite with Matchers:
   test("retry"):
     true
