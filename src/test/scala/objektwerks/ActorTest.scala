8b058d5cb0cfa15bea49492885839822aa430294
objektwerks <objektwerks@runbox.com>
2024-04-01 17:12:00 -0400

    edited actor test

--- src/test/scala/objektwerks/ActorTest.scala
+++ src/test/scala/objektwerks/ActorTest.scala
@@ -1,6 +1,7 @@
 package objektwerks
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
 class ActorTest extends AnyFunSuite with Matchers:
   test("actor"):
