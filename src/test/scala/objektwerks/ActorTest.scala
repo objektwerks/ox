b3fcbbdfa5fdf89540b597731c9148691b81cf3a
objektwerks <objektwerks@runbox.com>
2024-04-01 17:14:39 -0400

    enhanced actor test

--- src/test/scala/objektwerks/ActorTest.scala
+++ src/test/scala/objektwerks/ActorTest.scala
@@ -2,6 +2,12 @@ package objektwerks
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import java.util.concurrent.atomic.AtomicInteger
+
+
+class StatefulActor:
+  val counter = AtomicInteger(0)
+  def increment(delta: Int): Int = counter.addAndGet(delta)
 
 class ActorTest extends AnyFunSuite with Matchers:
   test("actor"):
