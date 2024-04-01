c0f3b7687f15b89c8a1e553ba991499a97e551af
objektwerks <objektwerks@runbox.com>
2024-04-01 14:05:07 -0400

    enhanced retry test

--- src/test/scala/objektwerks/RetryTest.scala
+++ src/test/scala/objektwerks/RetryTest.scala
@@ -3,6 +3,12 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import ox.*
+import ox.retry.{retry, RetryPolicy}
+
+import FileLineCount.*
+
 final class RetryTest extends AnyFunSuite with Matchers:
   test("retry"):
-    true
+    val fileLineCount =  retry( countFileLines(aFile) )( RetryPolicy.immediate(2) )
+    fileLineCount shouldBe aFileLineCount
