217ba825aeb11f8a34ddc9ebd59ea14f33b46eec
objektwerks <objektwerks@runbox.com>
2024-04-01 14:08:32 -0400

    edited retry test

--- src/test/scala/objektwerks/RetryTest.scala
+++ src/test/scala/objektwerks/RetryTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 import ox.*
-import ox.retry.{retry, RetryPolicy}
+import ox.retry.{retry, retryEither, RetryPolicy}
 
 import FileLineCount.*
 
@@ -12,3 +12,7 @@ final class RetryTest extends AnyFunSuite with Matchers:
   test("retry"):
     val fileLineCount =  retry( countFileLines(aFile) )( RetryPolicy.immediate(2) )
     fileLineCount shouldBe aFileLineCount
+
+  test("retry either"):
+    val fileLineCount =  retryEither( Right( countFileLines(aFile) ) )( RetryPolicy.immediate(2) )
+    fileLineCount.getOrElse(0) shouldBe aFileLineCount
