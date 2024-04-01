9225536adcbe60c9711694a80e19de78934f1aba
objektwerks <objektwerks@runbox.com>
2024-04-01 12:31:45 -0400

    edited timeout test

--- src/test/scala/objektwerks/TimeoutTest.scala
+++ src/test/scala/objektwerks/TimeoutTest.scala
@@ -1,17 +1,16 @@
 package objektwerks
 
-import java.util.concurrent.TimeoutException
-
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 import ox.*
 
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.TimeoutException
 
 import objektwerks.Fibonacci.fibonacci
 
 final class TimeoutTest extends AnyFunSuite with Matchers:
   test("timeout"):
-    val exception = intercept[TimeoutException]( timeout(10.micros)(fibonacci(96)) )
+    val exception = intercept[TimeoutException]( timeout(10.milliseconds)(fibonacci(96)) )
     exception shouldBe a[TimeoutException]
