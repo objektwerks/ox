61149bec90df75187a6947405d15ea2c83209f8c
objektwerks <objektwerks@runbox.com>
2024-04-01 14:17:06 -0400

    added resource test

---
+++ src/test/scala/objektwerks/ResourceTest.scala
@@ -0,0 +1,10 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import ox.*
+
+final class ResourceTest extends AnyFunSuite with Matchers:
+  test("resource"):
+    true
