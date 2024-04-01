82203d22d49bfa5dd65d4b0e15e53b9245659c87
objektwerks <objektwerks@runbox.com>
2024-04-01 12:41:57 -0400

    refactored timeout test

--- src/test/scala/objektwerks/TimeoutTest.scala
+++ src/test/scala/objektwerks/TimeoutTest.scala
@@ -6,11 +6,10 @@ import org.scalatest.matchers.should.Matchers
 import ox.*
 
 import scala.concurrent.duration.DurationInt
-import scala.concurrent.TimeoutException
 
-import objektwerks.Fibonacci.fibonacci
+import FileLineCount.*
 
 final class TimeoutTest extends AnyFunSuite with Matchers:
   test("timeout"):
-    val exception = intercept[TimeoutException]( timeout(10.milliseconds)(fibonacci(96)) )
-    exception shouldBe a[TimeoutException]
+    val result = timeoutOption(10.milliseconds) { countFileLines(aFile) }
+    result shouldBe None
