a2ef52da4d4abd6f0c5e56dc7ae6300ffe4a00ac
objektwerks <objektwerks@runbox.com>
2024-04-01 17:24:53 -0400

    enhanced actor test

--- src/test/scala/objektwerks/ActorTest.scala
+++ src/test/scala/objektwerks/ActorTest.scala
@@ -1,14 +1,24 @@
 package objektwerks
 
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers
 import java.util.concurrent.atomic.AtomicInteger
 
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-class StatefulActor:
-  val counter = AtomicInteger(0)
-  def increment(delta: Int): Int = counter.addAndGet(delta)
+import ox.*
+import ox.channels.Actor
+
+class Counter:
+  private val count = AtomicInteger(0)
+  def increment(number: Int): Int = count.addAndGet(number)
+  def value: Int = count.get()
 
 class ActorTest extends AnyFunSuite with Matchers:
-  test("actor"):
-    true
+  test("actor > ask"):
+    val count = supervised:
+      val actor = Actor.create(Counter())
+      actor.ask(_.increment(1))
+      actor.ask(_.increment(2))
+      actor.ask(_.increment(3))
+      actor.ask(_.value)
+    count shouldBe 6
