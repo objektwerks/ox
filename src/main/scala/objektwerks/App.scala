64b93fbcc4513085a2a2c3cf4a4636eb26ef61b3
objektwerks <objektwerks@runbox.com>
2024-04-26 12:41:02 -0400

    renamed App to ParApp

--- src/main/scala/objektwerks/App.scala
+++
@@ -1,6 +0,0 @@
-package objektwerks
-
-import ox.*
-
-@main
-def runApp: Unit = par( getJoke(), getJoke(), getJoke() ).toList.foreach(println)
---
+++ src/main/scala/objektwerks/ParApp.scala
@@ -0,0 +1,6 @@
+package objektwerks
+
+import ox.*
+
+@main
+def runParApp: Unit = par( getJoke(), getJoke(), getJoke() ).toList.foreach(println)
