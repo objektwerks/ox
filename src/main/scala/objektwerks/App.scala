c055358f3eaee5d076fdc93b3cb952e15afca21e
objektwerks <objektwerks@runbox.com>
2024-04-26 07:22:45 -0400

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,11 +1,17 @@
 package objektwerks
 
+import ox.*
+
 import scala.io.{Codec, Source}
 import scala.util.Using
 
 @main
 def runApp: Unit =
-  val joke = Using( Source.fromURL("https://api.chucknorris.io/jokes/random", Codec.UTF8.name) ) { 
+  val (joke1, joke2) = par( getJoke(), getJoke() )
+  println(joke1)
+  println(joke2)
+
+def getJoke(): String =
+  Using( Source.fromURL("https://api.chucknorris.io/jokes/random", Codec.UTF8.name) ) { 
     source => source.mkString
   }.get
-  println(joke)
