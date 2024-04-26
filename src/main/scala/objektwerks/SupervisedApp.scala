126ade60aa7726fe98fd729498fd2cca80489eda
objektwerks <objektwerks@runbox.com>
2024-04-26 12:51:32 -0400

    reformatted supervised app

--- src/main/scala/objektwerks/SupervisedApp.scala
+++ src/main/scala/objektwerks/SupervisedApp.scala
@@ -8,5 +8,9 @@ def runSupervisedApp: Unit =
     val joke1 = forkUser( getJoke() )
     val joke2 = forkUser( getJoke() )
     val joke3 = forkUser( getJoke() )
-    List( joke1.join(), joke2.join(), joke3.join() )
+    List(
+      joke1.join(),
+      joke2.join(),
+      joke3.join()
+    )
   jokes.foreach(println)
