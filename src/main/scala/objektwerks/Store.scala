28bed1c1ddfd7e0f4c7741b3de5c968d09915ce4
objektwerks <objektwerks@runbox.com>
2024-06-16 09:25:55 -0400

    added ox IO to store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -5,6 +5,8 @@ import com.zaxxer.hikari.HikariDataSource
 
 import javax.sql.DataSource
 
+import ox.IO
+
 import scalikejdbc.*
 
 final class Store(config: Config):
@@ -18,17 +20,17 @@ final class Store(config: Config):
   }
   ConnectionPool.singleton( DataSourceConnectionPool(dataSource) )
 
-  def addTodo(todo: Todo): Long =
+  def addTodo(todo: Todo)(using IO): Long =
     DB localTx { implicit session =>
       sql"insert into todo(task) values(${todo.task})".updateAndReturnGeneratedKey()
     }
 
-  def updateTodo(todo: Todo): Int =
+  def updateTodo(todo: Todo)(using IO): Int =
     DB localTx { implicit session =>
       sql"update todo set task = ${todo.task} where id = ${todo.id}".update()
     }
 
-  def listTodos(): Seq[Todo] =
+  def listTodos()(using IO): Seq[Todo] =
     DB readOnly { implicit session =>
       sql"select * from todo"
         .map(rs => Todo( rs.int("id"), rs.string("task") ) )
