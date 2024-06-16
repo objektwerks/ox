6694a46241f81432619401f4c615478aec3db852
objektwerks <objektwerks@runbox.com>
2024-06-16 08:04:58 -0400

    added store

---
+++ src/main/scala/objektwerks/Store.scala
@@ -0,0 +1,12 @@
+package objektwerks
+
+final class Store():
+  private val dataSource: DataSource = {
+    val ds = HikariDataSource()
+    ds.setDataSourceClassName(config.getString("db.driver"))
+    ds.addDataSourceProperty("url", config.getString("db.url"))
+    ds.addDataSourceProperty("user", config.getString("db.user"))
+    ds.addDataSourceProperty("password", config.getString("db.password"))
+    ds
+  }
+  ConnectionPool.singleton( DataSourceConnectionPool(dataSource) )
