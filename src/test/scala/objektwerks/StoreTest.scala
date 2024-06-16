d6950d56475fed67446b5b5db0bb66064d9328e4
objektwerks <objektwerks@runbox.com>
2024-06-16 08:55:50 -0400

    enhanced store test

--- src/test/scala/objektwerks/StoreTest.scala
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -3,8 +3,14 @@ package objektwerks
 import com.typesafe.config.ConfigFactory
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-final class StoreTest extends AnyFunSuite:
+import scala.sys.process.Process
+
+final class StoreTest extends AnyFunSuite with Matchers:
+  val exitCode = Process("psql -d house -f ddl.sql").run().exitValue()
+  exitCode shouldBe 0
+
   val store = Store( ConfigFactory.load("test.conf") )
 
   test("store"):
