11414ce824ef5c92892a9dede9c8eb6835d12cd8
objektwerks <objektwerks@runbox.com>
2024-06-16 08:43:32 -0400

    enhanced store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -17,3 +17,20 @@ final class Store(config: Config):
     ds
   }
   ConnectionPool.singleton( DataSourceConnectionPool(dataSource) )
+
+  def addTodo(todo: Todo): Long =
+    DB localTx { implicit session =>
+      sql"insert into todo(task) values(${todo.task})".updateAndReturnGeneratedKey()
+    }
+
+  def updateTodo(todo: Todo): Int =
+    DB localTx { implicit session =>
+      sql"update todo set task = ${todo.task} where id = ${todo.id}".update()
+    }
+
+  def listTodos(): Seq[Todo] =
+    DB readOnly { implicit session =>
+      sql"select * from todo"
+        .map(rs => Todo( rs.int("id"), rs.string("task") ) )
+        .list()
+    }
