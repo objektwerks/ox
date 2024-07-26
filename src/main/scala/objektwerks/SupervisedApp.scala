90abb61c70cb15ec120b5992875381f66d05f998
objektwerks <objektwerks@runbox.com>
2024-07-26 10:53:06 -0400

    removed explicit IO.unsafe

--- src/main/scala/objektwerks/SupervisedApp.scala
+++ src/main/scala/objektwerks/SupervisedApp.scala
@@ -6,13 +6,12 @@ import ox.*
   * See: https://ox.softwaremill.com/latest/structured-concurrency/fork-join.html
   */
 object SupervisedApp extends OxApp:
-  def run(args: Vector[String])(using Ox): ExitCode =
-    IO.unsafe:
-      supervised:
-        List(
-          forkUser( getJoke() ).join(),
-          forkUser( getJoke() ).join(),
-          forkUser( getJoke() ).join()
-        )
-      .foreach(println)
+  def run(args: Vector[String])(using Ox, IO): ExitCode =
+    supervised:
+      List(
+        forkUser( getJoke() ).join(),
+        forkUser( getJoke() ).join(),
+        forkUser( getJoke() ).join()
+      )
+    .foreach(println)
     ExitCode.Success
