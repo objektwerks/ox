861684515e07ef439935192e0f6db1b0fa8e93bf
objektwerks <objektwerks@runbox.com>
2024-07-26 10:52:24 -0400

    removed explicit IO.unsafe

--- src/main/scala/objektwerks/ParApp.scala
+++ src/main/scala/objektwerks/ParApp.scala
@@ -6,13 +6,12 @@ import ox.*
   * See: https://ox.softwaremill.com/latest/high-level-concurrency/par.html
   */
 object ParApp extends OxApp:
-  def run(args: Vector[String])(using Ox): ExitCode =
-    IO.unsafe:
-      par(
-        getJoke(),
-        getJoke(),
-        getJoke()
-      )
-      .toList
-      .foreach(println)
+  def run(args: Vector[String])(using Ox, IO): ExitCode =
+    par(
+      getJoke(),
+      getJoke(),
+      getJoke()
+    )
+    .toList
+    .foreach(println)
     ExitCode.Success
