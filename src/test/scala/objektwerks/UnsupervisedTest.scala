da325fc190cc81f072f595d7f0fdf7670fbbbd2a
objektwerks <objektwerks@runbox.com>
2024-08-19 14:30:15 -0400

    corrected unsupervised test

--- src/test/scala/objektwerks/UnsupervisedTest.scala
+++ src/test/scala/objektwerks/UnsupervisedTest.scala
@@ -13,8 +13,8 @@ import FileLineCount.*
   */
 final class UnsupervisedTest extends AnyFunSuite with Matchers:
   test("unsupervised > fork cancellable"):
-    val totalFileLineCount = unsupervised:
+    unsupervised:
       val aCountFileLinesFork = forkCancellable( countFileLines(aFile) )
       val bCountFileLinesFork = forkCancellable( countFileLines(bFile) )
-      aCountFileLinesFork.join() + bCountFileLinesFork.join()
-    totalFileLineCount shouldBe expectedFileLineCount
+      val totalFileLineCount = aCountFileLinesFork.join() + bCountFileLinesFork.join()
+      totalFileLineCount shouldBe expectedFileLineCount
