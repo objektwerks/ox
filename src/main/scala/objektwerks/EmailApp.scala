ab931ec7e0943a333149900f46955e3519c699bc
objektwerks <objektwerks@runbox.com>
2024-08-19 08:24:01 -0400

    refactored email app

--- src/main/scala/objektwerks/EmailApp.scala
+++ src/main/scala/objektwerks/EmailApp.scala
@@ -8,10 +8,13 @@ import ox.resilience.*
 object EmailApp extends OxApp:
   def run(args: Vector[String])(using Ox, IO): ExitCode =
     val config = ConfigFactory.load("email.conf")
+    val host = config.getString("email.host")
+    val sender = config.getString("email.sender")
+    val password = config.getString("email.password")
     val recipients = List( config.getString("email.sender") )
     val subject = config.getString("email.subject")
     val message = config.getString("email.message")
-    val emailer = Emailer(config)
+    val emailer = Emailer(host, sender, password)
 
     supervised:
       val either = retryEither( RetryConfig.immediate(2) )( Right( emailer.send(recipients, subject, message) ) )
