6f47d36bd028b5a37feb87ded8d7aab036c4d756
objektwerks <objektwerks@runbox.com>
2024-08-19 08:45:03 -0400

    enhanced email app

--- src/main/scala/objektwerks/EmailApp.scala
+++ src/main/scala/objektwerks/EmailApp.scala
@@ -7,9 +7,10 @@ import ox.resilience.*
 
 object EmailApp extends OxApp:
   def run(args: Vector[String])(using Ox, IO): ExitCode =
-    val emailer = Emailer( EmailServerConfig( ConfigFactory.load("email.conf") ) )
+    val config = ConfigFactory.load("email.conf")
+    val emailer = Emailer( EmailServerConfig(config) )
 
     supervised:
-      val either = retryEither( RetryConfig.immediate(2) )( Right( emailer.send( EmailConfig( ConfigFactory.load("email.conf") ) ) ) )
+      val either = retryEither( RetryConfig.immediate(2) )( Right( emailer.send( EmailConfig(config) ) ) )
       assert( either.isRight )
     ExitCode.Success
