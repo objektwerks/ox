790119801efa35cefe433257def032a10b70cb62
objektwerks <objektwerks@runbox.com>
2024-08-19 14:47:33 -0400

    refactored par test

--- src/test/scala/objektwerks/ParTest.scala
+++ src/test/scala/objektwerks/ParTest.scala
@@ -15,40 +15,47 @@ import FileLineCount.*
   */
 final class ParTest extends AnyFunSuite with Matchers:
   test("par"):
-    val (a, b) = par( countFileLines(aFile), countFileLines(bFile) )
-    a shouldBe aFileLineCount
-    b shouldBe bFileLineCount
-    a + b shouldBe expectedFileLineCount
+    supervised:
+      val (a, b) = par( countFileLines(aFile), countFileLines(bFile) )
+      a shouldBe aFileLineCount
+      b shouldBe bFileLineCount
+      a + b shouldBe expectedFileLineCount
 
   test("par limit"):
-    val functions = (1 to 3).map(n => () => n * n)
-    val results = parLimit(2)(functions)
-    results.sum shouldBe 14
+    supervised:
+      val functions = (1 to 3).map(n => () => n * n)
+      val results = parLimit(2)(functions)
+      results.sum shouldBe 14
 
   test("par either"):
-    val result = parEither(
-      Right( countFileLines(aFile) ),
-      Left(-1)    
-    )
-    result.map { (count, _) => count shouldBe aFileLineCount }
+    supervised:
+      val result = parEither(
+        Right( countFileLines(aFile) ),
+        Left(-1)
+      )
+      result.map { (count, _) => count shouldBe aFileLineCount }
 
   test("map par"):
-    val numbers = List(1, 2, 3)
-    val result = numbers.mapPar(2)( n => n * n )
-    result.sum shouldBe 14
+    supervised:
+      val numbers = List(1, 2, 3)
+      val result = numbers.mapPar(2)( n => n * n )
+      result.sum shouldBe 14
 
   test("foreach par"):
-    val numbers = List(1, 2, 3)
-    numbers.foreach( n => assert( n >= 1 ) )
+    supervised:
+      val numbers = List(1, 2, 3)
+      numbers.foreach( n => assert( n >= 1 ) )
 
   test("filter par"):
-    val numbers = List(1, 2, 3)
-    val result = numbers.filterPar(2)(_ % 2 == 0)
-    result.sum shouldBe 2
+    supervised:
+      val numbers = List(1, 2, 3)
+      val result = numbers.filterPar(2)(_ % 2 == 0)
+      result.sum shouldBe 2
 
   test("collect par"):
-    val input = List(1, 2, 3)
-    val result = input.collectPar(2) {
-      case i if i % 2 == 0 => i * i
-    } 
-    result.sum shouldBe 4
+    supervised:
+      val input = List(1, 2, 3)
+      val result = input.collectPar(2) {
+        case i if i % 2 == 0 => i * i
+      }
+      result.sum shouldBe 4
