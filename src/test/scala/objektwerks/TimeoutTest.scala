fa002c95a720aab6a6a2a4b1dc0e7b824f3fbed2
objektwerks <objektwerks@runbox.com>
2024-08-19 14:31:50 -0400

    corrected timeout test

--- src/test/scala/objektwerks/TimeoutTest.scala
+++ src/test/scala/objektwerks/TimeoutTest.scala
@@ -15,9 +15,11 @@ import FileLineCount.*
   */
 final class TimeoutTest extends AnyFunSuite with Matchers:
   test("timeout either"):
-    val result = timeoutEither(10.milliseconds, 0) { Right( countFileLines(aFile) ) }
-    result.isLeft shouldBe true
+    supervised:
+      val result = timeoutEither(10.milliseconds, 0) { Right( countFileLines(aFile) ) }
+      result.isLeft shouldBe true
 
   test("timeout option"):
-    val result = timeoutOption(10.milliseconds) { countFileLines(aFile) }
-    result shouldBe None
+    supervised:
+      val result = timeoutOption(10.milliseconds) { countFileLines(aFile) }
+      result shouldBe None
