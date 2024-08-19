d2aa57ed3483be188f044b5a107fe03a9109b1e9
objektwerks <objektwerks@runbox.com>
2024-08-19 14:36:00 -0400

    refactored supervised test

--- src/test/scala/objektwerks/SupervisedTest.scala
+++ src/test/scala/objektwerks/SupervisedTest.scala
@@ -17,18 +17,20 @@ import FileLineCount.*
   */
 final class SupervisedTest extends AnyFunSuite with Matchers:
   test("supervised > fork user"):
-    val totalFileLineCount = supervised:
-      val aCountFileLinesFork = forkUser( countFileLines(aFile) )
-      val bCountFileLinesFork = forkUser( countFileLines(bFile) )
-      aCountFileLinesFork.join() + bCountFileLinesFork.join()
+    val totalFileLineCount =
+      supervised:
+        val aCountFileLinesFork = forkUser( countFileLines(aFile) )
+        val bCountFileLinesFork = forkUser( countFileLines(bFile) )
+        aCountFileLinesFork.join() + bCountFileLinesFork.join()
     totalFileLineCount shouldBe expectedFileLineCount
 
   test("supervised > fork user > exception"):
-    val exception = intercept[FileNotFoundException] (
-      supervised:
-        val countFileLinesFork = forkUser( countFileLines("non.existent.file") )
-        countFileLinesFork.join()
-    )
+    val exception =
+      intercept[FileNotFoundException] (
+        supervised:
+          val countFileLinesFork = forkUser( countFileLines("non.existent.file") )
+          countFileLinesFork.join()
+      )
     exception shouldBe a[FileNotFoundException]
 
   test("supervised > fork user > catching"):
@@ -41,7 +43,8 @@ final class SupervisedTest extends AnyFunSuite with Matchers:
 
 
   test("supervised error > fork user error"):
-    val result = supervisedError(EitherMode[Int]):
-      forkUserError { Left(10) }
-      Right(())
+    val result =
+      supervisedError(EitherMode[Int]):
+        forkUserError { Left(10) }
+        Right(())
     result shouldBe Left(10)
