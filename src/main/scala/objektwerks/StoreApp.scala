46b018ada9cf836f95abaa8951a54743e6fb1487
objektwerks <objektwerks@runbox.com>
2024-08-20 13:29:23 -0400

    enhanced store app

--- src/main/scala/objektwerks/StoreApp.scala
+++ src/main/scala/objektwerks/StoreApp.scala
@@ -7,11 +7,14 @@ import ox.resilience.*
 
 object StoreApp extends OxApp:
   def run(args: Vector[String])(using Ox, IO): ExitCode =
-    val store = Store( StoreConfig( ConfigFactory.load("test.conf") ) )
+    val store = Store( StoreConfig( ConfigFactory.load("store.conf") ) )
     val todo = Todo(task = "Drink Dogfishhead 60 Minute IPA!")
 
     supervised:
-      val either = retryEither( RetryConfig.immediate(2) )( Right( store.addTodo(todo) ) )
-      assert( either.isRight )
+      val add = retryEither( RetryConfig.immediate(2) )( Right( store.addTodo(todo) ) )
+      assert( add.isRight )
+
+      val update = retryEither( RetryConfig.immediate(2) )( Right( store.updateTodo(todo.copy(task = "Drink DogfishHead 60 Minute IPA!")) ) )
+      assert( update.isRight )
 
     ExitCode.Success
