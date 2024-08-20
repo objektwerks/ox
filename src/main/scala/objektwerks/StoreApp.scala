b6b8c84252a25f58272e4c15ec5960ab8dbce57e
objektwerks <objektwerks@runbox.com>
2024-08-20 13:31:00 -0400

    enhanced store app

--- src/main/scala/objektwerks/StoreApp.scala
+++ src/main/scala/objektwerks/StoreApp.scala
@@ -17,4 +17,7 @@ object StoreApp extends OxApp:
       val update = retryEither( RetryConfig.immediate(2) )( Right( store.updateTodo(todo.copy(task = "Drink DogfishHead 60 Minute IPA!")) ) )
       assert( update.isRight )
 
+      val list = retryEither( RetryConfig.immediate(2) )( Right( store.listTodos() ) )
+      assert( list.isRight )
+
     ExitCode.Success
