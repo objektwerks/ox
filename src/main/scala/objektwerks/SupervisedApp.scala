d763b53520f2a3c566d38c61a3bc159e20968943
objektwerks <objektwerks@runbox.com>
2024-09-25 11:20:26 -0400

    removed io

--- src/main/scala/objektwerks/SupervisedApp.scala
+++ src/main/scala/objektwerks/SupervisedApp.scala
@@ -6,7 +6,7 @@ import ox.*
   * See: https://ox.softwaremill.com/latest/structured-concurrency/fork-join.html
   */
 object SupervisedApp extends OxApp:
-  def run(args: Vector[String])(using Ox, IO): ExitCode =
+  def run(args: Vector[String])(using Ox): ExitCode =
     supervised:
       List(
         forkUser( getJoke() ).join(),
