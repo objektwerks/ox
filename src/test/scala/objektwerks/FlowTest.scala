30599e0b02c9de7e01416278620b252d9c9783eb
objektwerks <objektwerks@runbox.com>
2024-10-21 14:44:12 -0400

    added flow test

---
+++ src/test/scala/objektwerks/FlowTest.scala
@@ -0,0 +1,8 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+final class FlowTest  extends AnyFunSuite with Matchers:
+  test("flow"):
+    true
