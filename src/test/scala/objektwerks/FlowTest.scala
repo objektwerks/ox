1246ef1ca5f917980b64776a89378ab73dbcb77e
objektwerks <objektwerks@runbox.com>
2024-10-21 15:40:39 -0400

    enhanced flow test

--- src/test/scala/objektwerks/FlowTest.scala
+++ src/test/scala/objektwerks/FlowTest.scala
@@ -8,8 +8,9 @@ import ox.flow.Flow
 final class FlowTest  extends AnyFunSuite with Matchers:
   test("flow"):
     val total = Flow
-      .fromValues(1, 2, 3)
+      .fromValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
       .map(i => i * i)
+      .mapPar(2)(i => i - 1)
       .runToList()
       .sum
-    total shouldBe 14
+    total shouldBe 375
