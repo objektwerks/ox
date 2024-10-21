586995164253ba7f7f7ab60c3a56e3479cdd19a1
objektwerks <objektwerks@runbox.com>
2024-10-21 15:11:56 -0400

    implemented flow test

--- src/test/scala/objektwerks/FlowTest.scala
+++ src/test/scala/objektwerks/FlowTest.scala
@@ -3,6 +3,13 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import ox.*
+import ox.flow.Flow
+
 final class FlowTest  extends AnyFunSuite with Matchers:
   test("flow"):
-    true
+    val result = Flow
+      .fromValues(1, 2, 3)
+      .map(i => i * i)
+      .runToList()
+    result.sum shouldBe 14
