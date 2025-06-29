e150e202eda5e2f233f219bb61669ef63bbf1f31
objektwerks <objektwerks@runbox.com>
2025-06-29 11:30:28 -0400

    removed otel from supervised app

--- src/main/scala/objektwerks/SupervisedApp.scala
+++ src/main/scala/objektwerks/SupervisedApp.scala
@@ -1,16 +1,11 @@
 package objektwerks
 
 import ox.{forkUser, ExitCode, Ox, OxApp, supervised}
-import ox.otel.context.PropagatingVirtualThreadFactory
 
 /**
   * See: https://ox.softwaremill.com/latest/structured-concurrency/fork-join.html
   */
 object SupervisedApp extends OxApp:
-  override def settings: OxApp.Settings = OxApp.Settings.Default.copy(
-    threadFactory = Some(PropagatingVirtualThreadFactory())
-  )
-
   def run(args: Vector[String])(using Ox): ExitCode =
     supervised:
       List(
