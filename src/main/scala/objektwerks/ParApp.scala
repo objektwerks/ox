4d8247a86d1497be612c8791c7fff065f0407b51
objektwerks <objektwerks@runbox.com>
2025-06-29 07:34:32 -0400

    added otel to par app

--- src/main/scala/objektwerks/ParApp.scala
+++ src/main/scala/objektwerks/ParApp.scala
@@ -1,11 +1,16 @@
 package objektwerks
 
-import ox.*
+import ox.{ExitCode, Ox, OxApp, par, supervised}
+import ox.otel.context.PropagatingVirtualThreadFactory
 
 /**
   * See: https://ox.softwaremill.com/latest/high-level-concurrency/par.html
   */
 object ParApp extends OxApp:
+  override def settings: OxApp.Settings = OxApp.Settings.Default.copy(
+    threadFactory = Some(PropagatingVirtualThreadFactory())
+  )
+
   def run(args: Vector[String])(using Ox): ExitCode =
     supervised:
       par(
