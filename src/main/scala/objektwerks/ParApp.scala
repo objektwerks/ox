709cabb1650f436541d28a0253db182ad5bc6b67
objektwerks <objektwerks@runbox.com>
2025-06-29 11:29:34 -0400

    removed otel from par app

--- src/main/scala/objektwerks/ParApp.scala
+++ src/main/scala/objektwerks/ParApp.scala
@@ -1,16 +1,11 @@
 package objektwerks
 
 import ox.{ExitCode, Ox, OxApp, par, supervised}
-import ox.otel.context.PropagatingVirtualThreadFactory
 
 /**
   * See: https://ox.softwaremill.com/latest/high-level-concurrency/par.html
   */
 object ParApp extends OxApp:
-  override def settings: OxApp.Settings = OxApp.Settings.Default.copy(
-    threadFactory = Some(PropagatingVirtualThreadFactory())
-  )
-
   def run(args: Vector[String])(using Ox): ExitCode =
     supervised:
       par(
